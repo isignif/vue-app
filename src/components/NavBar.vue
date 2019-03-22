f55294fa5ba2d49bfc3034bef3e88064af9ffac8
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2019-03-22 08:39:07 +0100

    [+] Include a NavBar Object

---
+++ src/components/NavBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="navbar navbar-expand-md navbar-light bg-light header">
+    <div class="container">
+      <a href="/" class="logo_header">
+        <img src="./../assets/logo.svg" alt="" height="38" width="130" />
+      </a>
+      <button aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-target="#navbarCollapse" data-toggle="collapse" type="button">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="navbar-collapse collapse justify-content-between" id="navbarCollapse">
+        <!--/ ITEMS LEFT ALIGNED -->
+        <ul class="navbar-nav mr-auto">
+          <!-- <% if current_user %> -->
+          <li class="nav-item"><a href="/dashboard" class="nav-link">Dashboard</a></li>
+          <li class="nav-item"><a href="/acts/" class="nav-link">Mes actes</a></li>
+          <li class="nav-item"><a href="/messages/" class="nav-link">Mes messages</a></li>
+          <!-- <% end %> -->
+        </ul>
+        <!--/ ITEMS RIGHT ALIGNED -->
+        <ul class="navbar-nav">
+          <!-- <% if current_user %> -->
+
+          <!-- <% else %> -->
+          <li class="nav-item"><a href="/signup" class="nav-link">Se connecter</a></li>
+          <li class="nav-item"><a href="/signin" class="nav-link">S'inscrire</a></li>
+          <li class="nav-item"><a href="/acts/new" class="nav-link">Déposer un acte</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  props: {}
+}
+</script>
