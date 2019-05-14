9caffa0fc8137a965fd02953286416753fb10a9f
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2019-05-14 12:17:52 +0200

    [M] Fetch acts from API

--- src/components/ActsTable.vue
+++ src/components/ActsTable.vue
@@ -1,35 +1,63 @@
 <template>
   <v-card>
-    <v-card-title>
-      Liste des actes
-      <v-spacer></v-spacer>
-      <v-text-field
+    <Loader v-if="loading" />
+    <div v-else>
+      <v-card-title>
+        Liste des actes
+        <v-spacer></v-spacer>
+        <v-text-field
         v-model="search"
         append-icon="search"
         label="Search"
         single-line
         hide-details
-      ></v-text-field>
-    </v-card-title>
-  <v-data-table :headers="headers" :items="acts" class="elevation-1" :search="search">
-    <template v-slot:items="props">
-      <td>{{ props.item.reference }}</td>
-      <td>{{ props.item.act_type }}</td>
-      <td>{{ props.item.significations }}</td>
-      <td>{{ props.item.advocate }}</td>
-      <td>{{ props.item.bailiffs }}</td>
-      <td>{{ props.item.step }}</td>
-    </template>
-  </v-data-table>
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table :headers="headers" :items="acts" class="elevation-1" :search="search">
+        <template v-slot:items="props">
+          <td>{{ props.item.attributes.reference }}</td>
+          <td>{{ props.item.act_type }}</td>
+          <td>{{ props.item.significations }}</td>
+          <td>{{ props.item.advocate }}</td>
+          <td>{{ props.item.bailiffs }}</td>
+          <td>{{ props.item.step }}</td>
+        </template>
+      </v-data-table>
+    </div>
 </v-card>
 </template>
 
 <script>
+import Loader from './Loader'
+
+const axios = require('axios')
+const api_url = require('../config').api_url
+
 export default {
   name: 'ActsTable',
   props: {},
+  components: {
+    Loader
+  },
+  methods:{
+    fetch(){
+      axios.get(`${api_url}/acts`, { headers: { Authorization: this.$store.state.logged_user.persistence_token } })
+        .then(response => {
+          this.acts = response.data.data
+
+          this.loading = false
+        })
+        .catch(error => {
+          console.error(error)
+        })
+    }
+  },
+  mounted(){
+    this.fetch()
+  },
   data() {
     return {
+      loading: true,
       search: '',
       headers: [{
           text: "Référence de l'acte",
