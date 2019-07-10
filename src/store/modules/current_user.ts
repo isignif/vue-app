7da36ec0f8cc81f33c5b9e8af43c50dce74815cc
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2019-07-10 17:34:35 +0200

    Improve typescript definition

--- src/store/modules/current_user.ts
+++ src/store/modules/current_user.ts
@@ -1,59 +1,66 @@
-import jwt_decode from 'jwt-decode';
+import jwt_decode from "jwt-decode";
 
+interface CurrentUserState {
+  id?: Number;
+  email?: String;
+  firstname?: String;
+  lastname?: String;
+  token?: String;
+}
 
 // initial state
-const state = {
-  id: null,
-  email: null,
-  firstname: null,
-  lastname: null,
-  token: null,
-}
+const state: CurrentUserState = {
+  id: undefined,
+  email: undefined,
+  firstname: undefined,
+  lastname: undefined,
+  token: undefined
+};
 
 // getters
 const getters = {
-  id: state => state.id,
-  token: state => state.token,
-  completeName: (state, getters) => {
+  id: (state: CurrentUserState) => state.id,
+  token: (state: CurrentUserState) => state.token,
+  completeName: (state: CurrentUserState, getters: any) => {
     if (getters.isLogged == false) {
-      return '';
+      return "";
     }
-    
-    return `${state.firstname} ${state.lastname}`
+
+    return `${state.firstname} ${state.lastname}`;
   },
-  isLogged: state => {
-    return state.id !== null
+  isLogged: (state: CurrentUserState) => {
+    return state.id !== null;
   }
-}
+};
 
 // actions
 const actions = {
-  setToken({commit}, token) {
-    commit('SET_TOKEN', token)
+  setToken({ commit }, token: string) {
+    commit("SET_TOKEN", token);
   },
-  signout({commit}) {
-    commit('REMOVE_USER')
+  signout({ commit }) {
+    commit("REMOVE_USER");
   }
-}
+};
 
 // mutations
 const mutations = {
-  SET_TOKEN (state, token) {
-    state.token = token
+  SET_TOKEN(state: CurrentUserState, token: String) {
+    state.token = token;
 
-    const userData = jwt_decode(token)
-    state.id = userData.user_id
-    state.email = userData.email
-    state.firstname = userData.firstname
-    state.lastname = userData.lastname
+    const userData = jwt_decode(token);
+    state.id = userData.user_id;
+    state.email = userData.email;
+    state.firstname = userData.firstname;
+    state.lastname = userData.lastname;
   },
-  REMOVE_USER (state) {
-    state.id = null
-    state.email = null
-    state.firstname = null
-    state.lastname = null
+  REMOVE_USER(state: CurrentUserState) {
+    state.id = undefined;
+    state.email = undefined;
+    state.firstname = undefined;
+    state.lastname = undefined;
   }
-}
+};
 
 export default {
   namespaced: true,
@@ -61,4 +68,4 @@ export default {
   getters,
   actions,
   mutations
-}
+};
