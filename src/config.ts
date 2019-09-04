4e5cfea9262262e1442aa54f01096570d1bf9db2
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2019-09-04 13:37:29 +0200

    Set staging environment as API

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,4 @@
 module.exports = {
-  // TODO: set to production environment when required
-  // api_url: "https://test.isignif.fr/api/v1"
-  api_url: "http://localhost:3000/api/v1"
+  api_url: "https://test.isignif.fr/api/v1",// Test environment
+  // api_url: "https://isignif.fr/api/v1",// Production environment
 };
