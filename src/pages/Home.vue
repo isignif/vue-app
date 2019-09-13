3f454c68ed7a47f14355bb8ae2116160072a1f2a
Antoine Delettre <ant.delettre@gmail.com>
2019-09-13 20:34:32 +0200

    Corrections about pull request n10

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -46,27 +46,27 @@
     
 
     <div class="pa-5 home2">
-      <h2 class="pt-4">Notre fonctionnement est simple</h2>
+      <h2 class="pt-4 text-xs-center blue--text">Notre fonctionnement est simple</h2>
       <v-layout row align-center>
-        <v-flex lg class="pa-4" style="text-align: center">
+        <v-flex lg class="pa-4 text-xs-center">
           <div style="padding: 0 60px">
-            <img alt="Etape 1" src="../assets/monitors.png">
+            <img alt="Etape 1" class="mb-5" src="../assets/monitors.png">
             <h3>1 - Choisissez</h3>
             <p>vos adresses et l’acte à signifier. Recevez une première estimation du coût de signification</p>
           </div>
         </v-flex>
 
-        <v-flex lg class="pa-4" style="text-align: center">
+        <v-flex lg class="pa-4 text-xs-center">
           <div style="padding: 0 60px">
-            <img alt="Etape 2" src="../assets/idea.png">
+            <img alt="Etape 2" class="mb-5" src="../assets/idea.png">
             <h3>2 - Créez rapidement</h3>
             <p>votre dépôt d’acte. Notre plateforme attribue les missions de significations aux huissiers de justice compétents</p>
           </div>
         </v-flex>
 
-        <v-flex lg class="pa-4" style="text-align: center">
+        <v-flex lg class="pa-4 text-xs-center">
           <div style="padding: 0 60px">
-            <img alt="Etape 3" src="../assets/paperplane.png">
+            <img alt="Etape 3" class="mb-5" src="../assets/paperplane.png">
             <h3>3 - Suivez l’avancée</h3>
             <p>de la signification de vos actes. Suivi, alertes mail, évaluations.. Nous gérons toutes les formalités !</p>
           </div>
@@ -75,51 +75,51 @@
     </div>
 
 
-    <div class="pa-5 home3">
-      <h2 class="pt-4" style="line-height: 45px">Essayez iSignif gratuitement <br><small>ou passez au Premium pour une utilisation illimitée</small></h2>
+    <div class="pa-5 home3 text-xs-left">
+      <h2 class="pt-4 text-xs-center mb-4">Essayez iSignif gratuitement <br><small>ou passez au Premium pour une utilisation illimitée</small></h2>
       <v-layout row align-center>
 
-        <v-flex lg class="pa-4">
+        <v-flex lg class="pa-4 mycard">
           <div style="float: right">
-            <v-card style="padding: 20px; margin: 0 auto; text-align: left; box-shadow: none !important; -webkit-box-shadow: none !important">
-              <v-card-title style="font-weight: 100; font-size: 25px; padding-bottom: 0">iSignif Basique</v-card-title>
-              <v-card-text style="padding-top: 10px">
+            <v-card class="pa-3">
+              <v-card-title class="pb-0">iSignif Basique</v-card-title>
+              <v-card-text class="pt-2">
                 <h4>Gratuit</h4>
-                <hr>
+                <hr class="mt-3">
                 <br>
                 <p class="card-text">✔ Jusqu'à 50 demandes de significations / mois</p>
-                <p class="card-text" style="color: darkgrey ">✖ Destinataires illimités</p>
-                <p class="card-text" style="color: darkgrey ">✖ Nouvelles fonctionnalités</p>
+                <p class="card-text" style="color: darkgrey">✖ Destinataires illimités</p>
+                <p class="card-text" style="color: darkgrey">✖ Nouvelles fonctionnalités</p>
                 <p class="card-text" style="color: darkgrey">✖ Suivi optimal orienté logiciel SaaS</p>
                 <p class="card-text" style="color: darkgrey">✖ Construction libre du réseau d'huissiers de justice</p>
               </v-card-text>
-              <v-card-actions>
-                <v-btn style="margin: 0 auto" text class="primary freemium_btn">Inscrivez-vous</v-btn>
+              <v-card-actions class="text-xs-center">
+                <v-btn text class="primary freemium_btn ma-auto">Inscrivez-vous</v-btn>
               </v-card-actions>
             </v-card>
           </div>
         </v-flex>
 
-        <v-flex lg class="pa-4">
+        <v-flex lg class="pa-4 mycard">
           <div style="float: left">
-            <v-card style="padding: 20px; margin: 0 auto; text-align: left">
-              <v-card-title style="font-weight: 500; font-size: 27px; padding-bottom: 0; color: #2C94C8">iSignif PREMIUM</v-card-title>
-              <v-card-text style="padding-top: 10px">
-                <h4>7,99€ HT <small>/ mois</small></h4>
-                <hr>
-                <br>
-                <p class="card-text">✔ Demandes de significations illimitées</p>
-                <p class="card-text">✔ Destinataires illimités</p>
-                <p class="card-text">✔ Nouvelles fonctionnalités</p>
-                <p class="card-text">✔ Suivi optimal orienté logiciel SaaS</p>
-                <p class="card-text">✔ Construction libre du réseau d'huissiers de justice</p>
-                <p class="card-text">✔ Alertes SMS pour les actes urgents</p>
-              </v-card-text>
-              <v-card-actions>
-                <v-btn style="margin: 0 auto" text class="primary font-weight-bold">Souscrire
-                  <i aria-hidden="true" class="v-icon material-icons" style="margin-left: 5px; font-size: 15px">keyboard_arrow_right</i></v-btn>
-              </v-card-actions>
-            </v-card>
+          <v-card class="pa-3">
+            <v-card-title class="pb-0">iSignif PREMIUM</v-card-title>
+            <v-card-text class="pt-2">
+              <h4>7,99€ HT <small>/ mois</small></h4>
+              <hr class="mt-3">
+              <br>
+              <p class="card-text">✔ Demandes de significations illimitées</p>
+              <p class="card-text">✔ Destinataires illimités</p>
+              <p class="card-text">✔ Nouvelles fonctionnalités</p>
+              <p class="card-text">✔ Suivi optimal orienté logiciel SaaS</p>
+              <p class="card-text">✔ Construction libre du réseau d'huissiers de justice</p>
+              <p class="card-text">✔ Alertes SMS pour les actes urgents</p>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn class="primary font-weight-bold ma-auto" text>Souscrire
+              <i aria-hidden="true" class="v-icon material-icons ml-2" style="font-size: 15px">keyboard_arrow_right</i></v-btn>
+            </v-card-actions>
+          </v-card>
           </div>
         </v-flex>
 
@@ -128,15 +128,14 @@
 
 
     <div class="pa-5 home4">
-      <h2 class="pt-4">Ils nous font confiance</h2>
+      <h2 class="pt-4 text-xs-center mb-5">Ils nous font confiance</h2>
+      <br>
       <v-layout row align-center>
 
     <h2 class="pa-4" style="text-align: center">Les avantages de notre plateforme</h2>
 
-        <v-flex lg class="pa-4" style="text-align: center">
-          <div style="padding: 0 40px">
-            <img src="https://i0.wp.com/proformal.fr/wp-content/uploads/2019/01/proformal-liberez-vous-des-formalites-legales.png?fit=423%2C285&ssl=1" alt="Partenaire">
-          </div>
+        <v-flex lg class="pa-4 text-xs-center">
+          <img src="https://i0.wp.com/proformal.fr/wp-content/uploads/2019/01/proformal-liberez-vous-des-formalites-legales.png?fit=423%2C285&ssl=1" alt="Partenaire">
         </v-flex>
 
       </v-layout>
@@ -158,6 +157,8 @@ export default {
 };
 </script>
 <style lang="scss">
+@import "../assets/stylesheets/custom.scss";
+
 .container {
   padding: 0 !important;
   margin: 0 !important;
@@ -182,91 +183,64 @@ h2{
   }
   .home2{
     h2{
-      text-align: center;
-      font-size: 2.5em;
-      text-transform: uppercase;
-      color: #2C94C8;
-      font-weight: normal;
       margin-bottom: 100px;
     }
     img{
       max-width: 120px;
-      margin-bottom: 20px;
-    }
-    h3{
-      font-size: 20px;
-    }
-    p{
-      font-size: 15px;
     }
   }
   .home3{
-    background: #f4f4f4;
-    text-align: left;
-    font-size: 16px;
+    background: $grey;
     background-image: url('../assets/bckTarif.png');
     background-repeat: no-repeat;
     background-size: 45%;
     background-position: 100% 100%;
     h2{
-      text-align: center;
-      font-size: 2.5em;
-      text-transform: uppercase;
-      font-weight: normal;
-      margin-bottom: 50px;
       small {
-        font-weight: 100;
-        font-size: 20px;
+        font-size: 1.4rem;
       }
     }
-    h4 {
-      font-size: 30px;
+    h4{
       small{
         color: grey;
-        font-size: 14px;
-        font-weight: normal
+        font-size: 0.9rem;
+        font-weight: normal;
+      }
+    }
+    .mycard{
+      &:first-child{
+        margin: 0 auto;
+        text-align: left;
+        box-shadow: none !important;
+        -webkit-box-shadow: none !important;
+        .v-card__title{
+          font-weight: 100;
+          font-size: 25px;
+        }
+      }
+      &:last-child{
+        margin: 0 auto;
+        text-align: left;
+        .v-card__title{
+          font-weight: 500;
+          font-size: 27px;
+          color: $blue;
+        }
       }
     }
     hr{
-      margin-top: 1rem;
-      margin-bottom: 1rem;
       border: 0;
       border-top: 1px solid rgba(0,0,0,0.1);
     }
     .freemium_btn{
       background: transparent !important;
-      border: 1px solid #2C94C8 !important;
-      color: #2C94C8 !important;
+      border: 1px solid $blue !important;
+      color: $blue !important;
     }
   }
   .home4{
-    h2{
-      text-align: center;
-      font-size: 2.5em;
-      text-transform: uppercase;
-      font-weight: normal;
-      margin-bottom: 50px;
-    }
     img{
       max-width: 200px;
     }
   }
-  // .video-container {
-  //   position: relative;
-  //   padding-bottom: 56.25%;
-  //   padding-top: 30px;
-  //   height: 0;
-  //   overflow: hidden;
-  //   max-width: 1500px;
-  //   margin: 50px auto;
-  // }
-  // .video-container iframe,  
-  // .video-container object,  
-  // .video-container embed {
-  //   position: absolute;
-  //   top: 0;
-  //   left: 0;
-  //   width: 100%;
-  //   height: 100%;
-  // }
 </style>
