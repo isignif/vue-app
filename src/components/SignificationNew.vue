18cbac67d79cc246a5b44251f6604492f4a5eb0a
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2019-09-14 14:53:05 +0200

    Improve responsive

--- src/components/SignificationNew.vue
+++ src/components/SignificationNew.vue
@@ -1,9 +1,9 @@
 <template>
-  <v-layout row class="elevation-3 pa-2 my-4 mx-2" align-center>
-    <v-flex xs5 class="px-2">
+  <v-layout row wrap class="elevation-3 pa-2 my-4 mx-2" align-center>
+    <v-flex sm5 xs12 class="px-2">
       <v-text-field v-model="name" label="Destinataire" prepend-icon="place"></v-text-field>
     </v-flex>
-    <v-flex xs6 fill-height class="px-2">
+    <v-flex sm6 xs12 fill-height class="px-2">
       <TownSelect v-model="townId" />
     </v-flex>
     <v-flex xs1 class="text-xs-right px-2" fill-height>
