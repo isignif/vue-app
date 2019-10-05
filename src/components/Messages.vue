0a2906278727e0661e32413c8d495b8f92e02092
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2019-10-05 16:41:15 +0200

    Migrate message

--- src/components/Messages.vue
+++ src/components/Messages.vue
@@ -1,21 +1,16 @@
 <template>
   <div>
     <Loader v-if="loading" />
-    <v-card
-        class="ma-2"
-        v-for="message in messages"
-        :key="message.id"
-        max-width="400"
-      >
+    <v-card class="ma-2" v-for="message in messages" :key="message.id" max-width="400">
       <v-card-title class="grey--text py-0">AUTHOR HERE</v-card-title>
-      <v-card-text class="d-flex">
-        {{ message.attributes.content }}
-      </v-card-text>
+      <v-card-text class="d-flex">{{ message.content }}</v-card-text>
     </v-card>
   </div>
 </template>
 
 <script>
+import { Message } from "../models/Message";
+
 export default {
   name: "Messages",
   props: {
@@ -27,24 +22,29 @@ export default {
     messages: []
   }),
   watch: {
-    significationId() { this.fetch(); },
+    significationId() {
+      this.fetch();
+    }
   },
   methods: {
     fetch() {
       this.loading = true;
-      this.$http
-        .get(`acts/${this.actId}/significations/${this.significationId}/messages`, {
-          headers: {
-            Authorization: this.$store.state.currentUser.token
-          }
-        })
-        .then(response => this.messages = response.data.data)
-        .catch(e => this.$toast.error(`Impossible de récupérrer les messages (${e.message})`))
-        .finally(() => this.loading = false);
+      Message.fromSignification(
+        this.actId,
+        this.significationId,
+        this.$store.state.currentUser.token
+      )
+        .then(messages => (messages = this.messages))
+        .catch(e =>
+          this.$toast.error(
+            `Impossible de récupérrer les messages (${e.message})`
+          )
+        )
+        .finally(() => (this.loading = false));
     }
   },
   mounted() {
     this.fetch();
-  },
+  }
 };
 </script>
