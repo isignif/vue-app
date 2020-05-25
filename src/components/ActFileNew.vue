25ca5aa410ebb2fcb82949f16e20797ce90eef3d
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2020-05-25 18:10:51 +0200

    Correct actFile create

--- src/components/ActFileNew.vue
+++ src/components/ActFileNew.vue
@@ -1,18 +1,16 @@
 <template>
   <v-layout row wrap class="pa-2 my-4 mx-2" align-center>
-    {{ actFile }}
-      <v-text-field v-model="actFile.name" label="Nom" v-if="actFile.files" required></v-text-field>
-      <input v-show="false" type="file" ref="inputUpload" @change="onFileChange" />
-      <v-btn text @click="actFile.files = null" v-if="actFile.files">
-        <v-icon>attachment</v-icon>&nbsp;{{ actFiles.files }}
+      <v-text-field v-model="name" label="Nom" v-if="files" required></v-text-field>
+      <input v-show="false" type="file" multiple="true" ref="inputUpload" @change="onFileChange" />
+      <v-btn text @click="files = null" v-if="files">
+        <v-icon>attachment</v-icon>&nbsp;{{ filesNames }}
       </v-btn>
       <v-btn text @click="$refs.inputUpload.click()" v-else>
         <v-icon>cloud_upload</v-icon>&nbsp; Ajouter une pièce jointe
       </v-btn>
       <v-btn color="primary"
           @click="onSubmit"
-          :disabled="!actFile.name || !actFile.files"
-          v-if="actFile.files">
+          v-if="files">
         <v-icon>check</v-icon>
         Confirmer
       </v-btn>
@@ -20,6 +18,7 @@
 </template>
 <script>
 import { ActFile } from 'isignif-client';
+import { mapState } from 'vuex';
 
 export default {
   name: "ActFileNew",
@@ -30,24 +29,55 @@ export default {
   data() {
     return {
       actFile: new ActFile(),
-      // name: "",
-      // files: null
+      name: "Document à signifier",
+      files: null
     };
+  },
+  computed: {
+    ...mapState('currentUser', {
+      token: state => state.token,
+    }),
+    filesNames: function() {
+      if (!this.files) return;
+
+      const names = [];
+
+      for (let i = 0; i < this.files.length; i++) {
+        const file = this.files.item(i);
+        names.push(file.name);
+      }
+
+      return names.join(', ');
+    }
   },
   methods: {
     onFileChange() {
-      console.log(this.$refs.inputUpload.files)
-      this.actFile.files = this.$refs.inputUpload.files;
+      this.files = this.$refs.inputUpload.files;
     },
     onSubmit() {
-      this.actFile.token = this.$store.state.currentUser.token;
+      const actFile = new ActFile();
+      actFile.token = this.token;
+      actFile.files = this.files;
+      actFile.name = this.name;
+
+      if (!actFile.files) {
+        return this.$toast.error("Vous n'avez pas attaché de fichiers.");
+      }
+      if (!actFile.name) {
+        return this.$toast.error("Vous devez spécifier un nom.");
+      }
 
       this.actFile
         .save()
         .then(response => {
+          this.$toast.success("Le fichier a été ajouté.");
           this.$emit("created", this.actFile);
         })
-        .catch(error => console.error(error));
+        .catch(error => {
+          console.error(error);
+          this.$toast.error("Le fichier n'a pas pu être ajouté.");
+
+        });
     }
   }
 };
